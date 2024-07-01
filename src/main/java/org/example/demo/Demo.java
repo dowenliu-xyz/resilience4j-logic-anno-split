8b7dc12afc21ff46eb9b058e2a32d01520d817cc
dowenliu-xyz <hawkdowen@hotmail.com>
2024-07-01 21:55:01 +0800

    add log

--- src/main/java/org/example/demo/Demo.java
+++ src/main/java/org/example/demo/Demo.java
@@ -1,14 +1,18 @@
 package org.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class Demo extends DemoParent implements DemoInterface {
     public String fallbackBySuper(String name, Throwable e) {
+        log.info("stack: ", e);
         return "bySuper:fallback";
     }
 
     public String fallbackByInterface(String name, Throwable e) {
+        log.info("stack: ", e);
         return "byInterface:fallback";
     }
 }
